78c0f1170084e5ef82e06ca47f223281198903ff
yashodhanAgale <yashodhanagale3@gmail.com>
2025-07-28 02:30:28 +0530

    modified base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance =axios.create({
-  baseURL: "https://note-taking-backend-j048upkg0-yashodhans-projects-7e631381.vercel.app/api",
+  baseURL: "https://note-taking-backend-wmdl.vercel.app/api",
   withCredentials: true,
 });
 
